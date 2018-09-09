900ec095642419c401d990bf0f8759b3a534a843
Mac Heller-Ogden <mac@machellerogden.com>
2018-09-08 22:58:35 -0500

    Gaining some structure

--- index.js
+++ index.js
@@ -2,103 +2,42 @@
 
 module.exports = load;
 
-const { VM } = require('vm2');
-const acorn = require('acorn');
-const astring = require('astring');
-const fs = require('fs');
 const path = require('path');
 const callsites = require('callsites');
-const YAML = require('js-yaml');
-const { inspect } = require('util');
 const globby = require('globby');
-const m = require('module');
 const untildify = require('untildify');
+const reader = require('./lib/reader');
+const parse = require('./lib/parse');
 
-const evaluate = (statement, sandbox) => new VM({ sandbox }).run(statement);
+function load(givenPatterns, searchFromRoot = false) {
 
-const cast = (obj) => {
-    if ('name' in obj) return {
-        type: 'Identifier',
-        name: obj.name
-    }
-    if ('value' in obj) return {
-        type: 'Literal',
-        value: obj.value
-    }
-};
-
-const isObject = v => v != null && typeof v === 'object' && !Array.isArray(v);
-
-function BinaryExpression(...args) {
-    if (args.length !== 3) throw new Error('BinaryExpression requires exactly 3 arguments');
-    const [ givenLeft, givenRight, operator = '==' ] = args;
-    const left = cast(givenLeft);
-    const right = cast(givenRight);
-    const expression = {
-        type: 'BinaryExpression',
-        left,
-        operator,
-        right
-    };
-    const statement = {
-        type: 'ExpressionStatement',
-        expression
-    };
-    const ast = {
-        type: 'Program',
-        body: [ statement ]
-    };
-    return astring.generate(ast);
-}
-
-const statements = {
-    BinaryExpression
-};
-
-function parse(given) {
-    if (Array.isArray(given)) {
-        return given.reduce((acc, value) => {
-            if (isObject(value) && typeof value.type === 'string' && Array.isArray(value.statement)) return [ ...acc, evaluate(statements[value.type](...value.statement), acc) ];
-            return [ ...acc, parse(value) ];
-        }, []);
-    }
-    if (given != null && typeof given === 'object') {
-        return Object.entries(given).reduce((acc, [ key, value ]) => {
-            if (isObject(value) && typeof value.type === 'string' && Array.isArray(value.statement)) {
-                acc[key] = evaluate(statements[value.type](...value.statement), acc);
-            } else {
-                acc[key] = parse(value);
-            }
-            return acc;
-        }, {});
-    }
-    return given;
-}
-
-const yml = (filePath) => YAML.safeLoad(fs.readFileSync(filePath, 'utf8'));
-const json = (filePath) => m._load(filePath);
-
-const readers = {
-    yml,
-    yaml: yml,
-    json
-};
-
-function load(givenPatterns, external = false) {
     const patterns = Array.isArray(givenPatterns)
         ? givenPatterns.map(p => untildify(p))
         : untildify(givenPatterns);
-    const dir = external
+
+    const dir = searchFromRoot
         ? '/'
         : path.dirname(callsites()[1].getFileName());
+
     const paths = globby.sync(patterns, { cwd: dir });
-    return paths.reduce((acc, file) => {
-        const extIdx = file.lastIndexOf('.');
-        const pdIdx = file.lastIndexOf(path.sep);
-        const name = file.slice(pdIdx < 0 ? 0 : pdIdx + 1, extIdx);
-        const ext = file.slice(extIdx + 1);
-        const filePath = path.join(dir, file);
-        if (Object.keys(readers).includes(ext)) acc[name] = parse(readers[ext](filePath));
+
+    return paths.reduce((acc, currentFile) => {
+
+        const file = {
+            ...path.parse(currentFile),
+            path: path.resolve(currentFile)
+        };
+
+        const key = file.ext.startsWith('.')
+            ? file.ext.slice(1)
+            : file.ext;
+
+        if (Object.keys(reader).includes(key)) acc.push({
+            name: file.name,
+            file: file.path,
+            data: parse(reader[key](file.path))
+        });
+
         return acc;
-    }, {});
+    }, []);
 }
---
+++ test/index.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const chai = require('chai');
+const { expect } = chai;
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+describe('#conform', () => {
+    
+});

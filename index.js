f240b7ec42e635f3f681efc140ebf3d32b8afe99
Mac Heller-Ogden <mac@machellerogden.com>
2018-09-10 09:28:14 -0500

    extracted loader to new package

--- index.js
+++ index.js
@@ -2,42 +2,12 @@
 
 module.exports = load;
 
-const path = require('path');
-const callsites = require('callsites');
-const globby = require('globby');
-const untildify = require('untildify');
-const reader = require('./lib/reader');
 const parse = require('./lib/parse');
+const Globble = require('globble');
 
 function load(givenPatterns, searchFromRoot = false) {
-
-    const patterns = Array.isArray(givenPatterns)
-        ? givenPatterns.map(p => untildify(p))
-        : untildify(givenPatterns);
-
-    const dir = searchFromRoot
-        ? '/'
-        : path.dirname(callsites()[1].getFileName());
-
-    const paths = globby.sync(patterns, { cwd: dir });
-
-    return paths.reduce((acc, currentFile) => {
-
-        const file = {
-            ...path.parse(currentFile),
-            path: path.resolve(currentFile)
-        };
-
-        const key = file.ext.startsWith('.')
-            ? file.ext.slice(1)
-            : file.ext;
-
-        if (Object.keys(reader).includes(key)) acc.push({
-            name: file.name,
-            file: file.path,
-            data: parse(reader[key](file.path))
-        });
-
-        return acc;
-    }, []);
+    return Globble(givenPatterns, searchFromRoot).map(({ data, ...rest }) => ({
+        ...rest,
+        data: parse(data)
+    }));
 }

6042f138bbacd1cfd059a2f7ff7507d695ba24fe
Mac Heller-Ogden <mac@machellerogden.com>
2018-09-17 22:30:28 -0500

    WIp

--- README.md
+++ README.md
@@ -4,4 +4,92 @@
 
 [![Version](https://img.shields.io/npm/v/joyce.svg)]() [![Travis](https://img.shields.io/travis/machellerogden/joyce.svg)]() [![License](https://img.shields.io/npm/l/joyce.svg)]()
 
-README is under development. Please refer to [tests](./test/index.spec.js) for usage until proper docs are in place.
+## Installation
+
+```sh
+npm i joyce
+```
+
+## Usage
+
+Joyce accepts any type of value. If that value happens to be or happens to contain a string which starts with a special prefix, then that string value will be parsed and evaluated within the context of the originally given value.
+
+### A Basic Example
+
+```js
+const Joyce = require('joyce');
+
+Joyce({
+    hello: 'world',
+    isWorld: 'Fn::Equals::Ref(hello)::String(world)'
+});
+
+// returns...
+{
+    hello: 'world',
+    isWorld: true
+}
+```
+
+### A Shorthand Example
+
+> Joyce is optimized for usablity. Here's the same examples from above but this time using a shorthand syntax.
+
+```js
+const Joyce = require('joyce');
+
+Joyce({
+    hello: 'world',
+    isWorld: 'Fn::Equals(hello)::String(world)'
+});
+
+// returns...
+{
+    hello: 'world',
+    isWorld: true
+}
+```
+
+### Kitchen Sink Example
+
+>
+
+```js
+const Joyce = require('joyce');
+
+Joyce({
+    foo: 'bar',
+
+    bar: 'Fn::Equals::Ref(foo)::String(bar)',
+
+    // when using shorthand... first argument becomes part of operation.
+    baz: 'Fn::Equals(bar)::Ref(foo)',
+
+    qux: [ 1, 2, 3, 4 ],
+
+    xyzzy: 'Fn::Filter::Op(gte)::Ref(qux)::Number(3)',
+
+// and the shorthand...
+    uuddlrlrba: 'Fn::Filter(gte)::Ref(qux)::Number(2)',
+
+    bim: 'Fn::Join::String(-)::Ref(qux)',
+
+    // shorthand again...
+    bam: 'Fn::Join(***)::Ref(qux)',
+
+    boom: 'Fn::Template(${0} is a ${1} name to use in an example)::Ref(foo)::String(terrible)'
+});
+
+// returns...
+{
+    foo: 'bar',
+    bar: true,
+    baz: true,
+    qux: [ 1, 2, 3, 4 ],
+    xyzzy: [ 3, 4 ],
+    uuddlrlrba: [ 2, 3, 4 ],
+    bim: '1-2-3-4',
+    bam: '1***2***3***4',
+    boom: 'bar is a terrible name to use in an example'
+}
+```
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,40 +3,69 @@
 const chai = require('chai');
 const { expect } = chai;
 
-const joyce = require('..');
+const Joyce = require('..');
 
-describe('joyce', () => {
+describe('Joyce', () => {
     it('works', () => {
-        expect(joyce({
+        expect(Joyce({
             foo: 'bar',
+
+            // long-form usage
             bar: 'Fn::Equals::Ref(foo)::String(bar)',
-            baz: {
-                qux: [
-                    'Fn::Equals::Ref(foo)::String(bar)'
-                ]
-            }
+
+            // shorthand usage
+            baz: 'Fn::Equals(bar)::Ref(foo)',
+
+            qux: [ 1, 2, 3, 4 ],
+
+            xyzzy: 'Fn::Filter::Op(gte)::Ref(qux)::Number(3)',
+
+            // same example using shorthand... first argument is becomes cast by the stated operation.
+            uuddlrlrba: 'Fn::Filter(gte)::Ref(qux)::Number(2)',
+
+            bim: 'Fn::Join::String(-)::Ref(qux)',
+
+            // shorthand again...
+            bam: 'Fn::Join(***)::Ref(qux)',
+
+            boom: 'Fn::Template(${0} is a ${1} name to use in an example)::Ref(foo)::String(terrible)'
         })).to.eql({
             foo: 'bar',
             bar: true,
-            baz: { qux: [ true ] }
+            baz: true,
+            qux: [ 1, 2, 3, 4 ],
+            xyzzy: [ 3, 4 ],
+            uuddlrlrba: [ 2, 3, 4 ],
+            bim: '1-2-3-4',
+            bam: '1***2***3***4',
+            boom: 'bar is a terrible name to use in an example'
         });
     });
+    it('and it works with strings... although, beats me why you would want context-less evaluation like this.', () => {
+        expect(Joyce('Fn::Equals::String(foo)::String(foo)')).to.eql(true);
+    });
+    it('and arrays... but again, why would anyone want this...', () => {
+        expect(Joyce([
+            'foo',
+            'Fn::Equals::Ref(0)::String(foo)'
+        ])).to.eql([ 'foo', true ]);
+    });
     it('has a customizable prefix and delimiter', () => {
-        expect(joyce({
+        expect(Joyce({
             foo: 'bar',
             bar: 'Magic!!!Equals::Ref(foo)::String(bar)'
         }, { prefix: 'Magic!!!' })).to.eql({
             foo: 'bar',
             bar: true
         });
-        expect(joyce({
+        expect(Joyce({
             foo: 'bar',
             bar: '@Equals::Ref(foo)::String(bar)'
         }, { prefix: '@' })).to.eql({
             foo: 'bar',
             bar: true
         });
-        expect(joyce({
+        expect(Joyce({
             foo: 'bar',
             bar: '@Equals/Ref(foo)/String(bar)'
         }, { prefix: '@', delim: '/' })).to.eql({
@@ -45,50 +74,89 @@ describe('joyce', () => {
         });
     });
     it('can filter things', () => {
-        expect(joyce({
+        expect(Joyce({
             foo: [ 1, 2, 3 ],
-            bar: 'Fn::Filter::Equals::Ref(foo)::Number(1)'
+            bar: 'Fn::Filter(Equals)::Ref(foo)::Number(1)'
         })).to.eql({
             foo: [ 1, 2, 3 ],
             bar: [ 1 ]
         });
-        expect(joyce({
-            bar: 'Fn::Filter::Eq::Array([1,2,3,4])::Number(1)'
+        expect(Joyce({
+            bar: 'Fn::Filter(Eq)::Array([1,2,3,4])::Number(1)'
         })).to.eql({
             bar: [ 1 ]
         });
-        expect(joyce({
-            bar: 'Fn::Filter::GT::Array([1,2,3,4])::Number(1)'
+        expect(Joyce({
+            bar: 'Fn::Filter(GT)::Array([1,2,3,4])::Number(1)'
         })).to.eql({
             bar: [ 2, 3, 4 ]
         });
-        expect(joyce({
-            bar: 'Fn::Filter::GTE::Array([1,2,3,4])::Number(1)'
+        expect(Joyce({
+            bar: 'Fn::Filter(GTE)::Array([1,2,3,4])::Number(1)'
         })).to.eql({
             bar: [ 1, 2, 3, 4 ]
         });
-        expect(joyce({
-            bar: 'Fn::Filter::LTE::Array([1,2,3,4])::Number(2)'
+        expect(Joyce({
+            bar: 'Fn::Filter(LTE)::Array([1,2,3,4])::Number(2)'
         })).to.eql({
             bar: [ 1, 2 ]
         });
-        expect(joyce({
-            bar: 'Fn::Filter::Mod::Array([1,2,3,4])::Number(2)'
+        expect(Joyce({
+            bar: 'Fn::Filter(Mod)::Array([1,2,3,4])::Number(2)'
+        })).to.eql({
+            bar: [ 1, 3 ]
+        });
+        expect(Joyce({
+            foo: [ 1, 2, 3 ],
+            bar: 'Fn::Filter::Op(Equals)::Ref(foo)::Number(1)'
+        })).to.eql({
+            foo: [ 1, 2, 3 ],
+            bar: [ 1 ]
+        });
+        expect(Joyce({
+            bar: 'Fn::Filter::Op(Eq)::Array([1,2,3,4])::Number(1)'
+        })).to.eql({
+            bar: [ 1 ]
+        });
+        expect(Joyce({
+            bar: 'Fn::Filter::Op(GT)::Array([1,2,3,4])::Number(1)'
+        })).to.eql({
+            bar: [ 2, 3, 4 ]
+        });
+        expect(Joyce({
+            bar: 'Fn::Filter::Op(GTE)::Array([1,2,3,4])::Number(1)'
+        })).to.eql({
+            bar: [ 1, 2, 3, 4 ]
+        });
+        expect(Joyce({
+            bar: 'Fn::Filter::Op(LTE)::Array([1,2,3,4])::Number(2)'
+        })).to.eql({
+            bar: [ 1, 2 ]
+        });
+        expect(Joyce({
+            bar: 'Fn::Filter::Op(Mod)::Array([1,2,3,4])::Number(2)'
         })).to.eql({
             bar: [ 1, 3 ]
         });
     });
     it('can join things', () => {
-        expect(joyce({
+        expect(Joyce({
             foo: [ 'foo', 'bar', 'baz' ],
             bar: 'Fn::Join::String(-)::Ref(foo)'
         })).to.eql({
             foo: [ 'foo', 'bar', 'baz' ],
             bar: 'foo-bar-baz'
         });
+        expect(Joyce({
+            foo: [ 'foo', 'bar', 'baz' ],
+            bar: 'Fn::Join(-)::Ref(foo)'
+        })).to.eql({
+            foo: [ 'foo', 'bar', 'baz' ],
+            bar: 'foo-bar-baz'
+        });
     });
     it('can resolve deep values', () => {
-        expect(joyce({
+        expect(Joyce({
             sis: {
                 boom: {
                     bah: [ 'foo', 'bar', 'baz' ]
@@ -105,7 +173,7 @@ describe('joyce', () => {
         });
     });
     it('can handle recursive evaluations in any order', () => {
-        expect(joyce({
+        expect(Joyce({
             foo: [ 'a', 'b', 'c' ],
             baz: 'Fn::Equals::Ref(bar)::String(a-b-c)',
             bar: 'Fn::Join::String(-)::Ref(foo)'
@@ -116,9 +184,16 @@ describe('joyce', () => {
         });
     });
     it('can do template strings... in it\'s own special way', () => {
-        expect(joyce({
+        expect(Joyce({
             foo: 'bar',
             baz: 'Fn::Template(foo-${0}-${2}-${1})::Ref(foo)::String(qux)::String(baz)'
+        })).to.eql({
+            foo: 'bar',
+            baz: 'foo-bar-baz-qux'
+        });
+        expect(Joyce({
+            foo: 'bar',
+            baz: 'Fn::Template::String(foo-${0}-${2}-${1})::Ref(foo)::String(qux)::String(baz)'
         })).to.eql({
             foo: 'bar',
             baz: 'foo-bar-baz-qux'

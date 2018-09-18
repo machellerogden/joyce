8e491b0e581982076477e5433597eb873479efb9
Mac Heller-Ogden <mac@machellerogden.com>
2018-09-18 09:13:56 -0500

    stdin support

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+#!/usr/bin/env node
 'use strict';
 
-module.exports = require('./lib/compile');
+const compile = require('./lib/compile');
+
+if (!process.stdin.isTTY) {
+    const data = require('fs').readFileSync(0, 'utf8');
+    if (data != null) {
+        try {
+            process.stdout.write(JSON.stringify(compile(JSON.parse(data)), null, 4));
+        } catch {
+            process.stderr.write('{"error":"invalid JSON"}');
+        }
+    }
+}
+
+module.exports = compile;

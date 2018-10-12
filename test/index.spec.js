719f2b2f5964c8dca6b522b735425bf6847bb851
Mac Heller-Ogden <mac@machellerogden.com>
2018-10-12 11:36:09 -0500

    update

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,7 +6,7 @@ const { expect } = chai;
 const Joyce = require('..');
 
 describe('Joyce', () => {
-    it('works', () => {
+    it('can compare values', () => {
         expect(Joyce({
             foo: 'bar',
             oof: 'true',
@@ -14,7 +14,6 @@ describe('Joyce', () => {
             baz: 100,
             qux: { "yes" : "no" },
             xyzzy: [ "sis", "boom ! ! !", "bah" ],
-            'strange))': "but true",
             a: '((== foo "bar"))',
             b: '((== a true))',
             c: '((=== foo "bar"))',
@@ -22,33 +21,7 @@ describe('Joyce', () => {
             e: '((> baz 5))',
             f: '((>= baz 5))',
             g: '((< baz 5))',
-            h: '((<= baz 5))',
-            i: '((% baz 5))',
-            j: '((+ baz 5))',
-            k: '((+ foo "-bar"))',
-            l: '((- baz 5))',
-            m: '((* baz 5))',
-            n: '((/ baz 5))',
-            o: '((filter >= bar 3))',
-            p: '((filter >= bar 2))',
-            q: '((join "-" bar))',
-            r: '((join "~~~" xyzzy))',
-            s: '((foo)), a "note" to follow (("foo"))',
-            t: '((every < bar 6))',
-            u: '((Every > bar 5))',
-            v: '((Some >= bar 5))',
-            w: '((Some > bar 5))',
-            x: '((Find == xyzzy "boom ! ! !"))',
-            y: '((Map + xyzzy ".png"))',
-            z: '((Sum bar))',
-            aa: '((Product bar))',
-            bb: '((== oof "true"))',
-            cc: '((join xyzzy))',
-            dd: '((join "))" xyzzy))',
-            ee: '((join "((foo))" xyzzy))',
-            ff: '((join ref("strange))") xyzzy))',
-            gg: [ 'berry', 'pie' ],
-            hh: 'foo ((join " " gg))',
+            h: '((<= baz 5))'
         })).to.eql({
             foo: 'bar',
             oof: 'true',
@@ -56,7 +29,6 @@ describe('Joyce', () => {
             baz: 100,
             qux: { "yes" : "no" },
             xyzzy: [ "sis", "boom ! ! !", "bah" ],
-            'strange))': "but true",
             a: true,
             b: true,
             c: true,
@@ -64,48 +36,39 @@ describe('Joyce', () => {
             e: true,
             f: true,
             g: false,
-            h: false,
+            h: false
+        });
+    });
+    it('can perform binary operations', () => {
+        expect(Joyce({
+            foo: 'bar',
+            oof: 'true',
+            bar: [ 1, 2, 3, 4, 5 ],
+            baz: 100,
+            qux: { "yes" : "no" },
+            xyzzy: [ "sis", "boom ! ! !", "bah" ],
+            i: '((% baz 5))',
+            j: '((+ baz 5))',
+            k: '((+ foo "-bar"))',
+            l: '((- baz 5))',
+            m: '((* baz 5))',
+            n: '((/ baz 5))'
+        })).to.eql({
+            foo: 'bar',
+            oof: 'true',
+            bar: [ 1, 2, 3, 4, 5 ],
+            baz: 100,
+            qux: { "yes" : "no" },
+            xyzzy: [ "sis", "boom ! ! !", "bah" ],
             i: 0,
             j: 105,
             k: 'bar-bar',
             l: 95,
             m: 500,
-            n: 20,
-            o: [ 3, 4, 5 ],
-            p: [ 2, 3, 4, 5 ],
-            q: '1-2-3-4-5',
-            r: 'sis~~~boom ! ! !~~~bah',
-            s: 'bar, a "note" to follow foo',
-            t: true,
-            u: false,
-            v: true,
-            w: false,
-            x: 'boom ! ! !',
-            y: [ 'sis.png', 'boom ! ! !.png', 'bah.png' ],
-            z: 15,
-            aa: 120,
-            bb: true,
-            cc: 'sisboom ! ! !bah',
-            dd: 'sis))boom ! ! !))bah',
-            ee: 'sis((foo))boom ! ! !((foo))bah',
-            ff: 'sisbut trueboom ! ! !but truebah',
-            gg: [ 'berry', 'pie' ],
-            hh: 'foo berry pie'
+            n: 20
         });
     });
-    it('and it works with strings...', () => {
-        expect(Joyce('((== "foo" "foo"))')).to.eql(true);
-        expect(Joyce('((== bam "boom"))', {
-            bam: 'boom'
-        })).to.eql(true);
-    });
-    it('and arrays... but again, why would anyone want this...', () => {
-        expect(Joyce([
-            'foo',
-            '((== ref(0) "foo"))'
-        ])).to.eql([ 'foo', true ]);
-    });
-    it('can filter things', () => {
+    it('can filter', () => {
         expect(Joyce({
             foo: [ 1, 2, 3 ],
             bar: '((filter == foo 1))'
@@ -146,7 +109,7 @@ describe('Joyce', () => {
             bar: [ 1 ]
         });
     });
-    it('can join things', () => {
+    it('can join', () => {
         expect(Joyce({
             foo: [ 'foo', 'bar', 'baz' ],
             bar: '((join "-" foo))'
@@ -160,6 +123,33 @@ describe('Joyce', () => {
         })).to.eql({
             foo: [ 'foo', 'bar', 'baz' ],
             bar: 'foo-bar-baz'
+        });
+        expect(Joyce({
+            foo: 'bar',
+            bar: [ 1, 2, 3, 4, 5 ],
+            baz: 100,
+            qux: { "yes" : "no" },
+            xyzzy: [ "sis", "boom ! ! !", "bah" ],
+            'strange))': "but true",
+            cc: '((join xyzzy))',
+            dd: '((join "))" xyzzy))',
+            ee: '((join "((foo))" xyzzy))',
+            ff: '((join ref("strange))") xyzzy))',
+            gg: [ 'berry', 'pie' ],
+            hh: 'foo ((join " " gg))'
+        })).to.eql({
+            foo: 'bar',
+            bar: [ 1, 2, 3, 4, 5 ],
+            baz: 100,
+            qux: { "yes" : "no" },
+            xyzzy: [ "sis", "boom ! ! !", "bah" ],
+            'strange))': "but true",
+            cc: 'sisboom ! ! !bah',
+            dd: 'sis))boom ! ! !))bah',
+            ee: 'sis((foo))boom ! ! !((foo))bah',
+            ff: 'sisbut trueboom ! ! !but truebah',
+            gg: [ 'berry', 'pie' ],
+            hh: 'foo berry pie'
         });
     });
     it('can resolve deep values', () => {
@@ -190,7 +180,7 @@ describe('Joyce', () => {
             bar: 'a-b-c'
         });
     });
-    it('can handle multiple expression in a single string', () => {
+    it('can handle multiple expression in a single string (templating)', () => {
         expect(Joyce({
             quake: 'daisy',
             daisy: 'sky',
@@ -211,5 +201,81 @@ describe('Joyce', () => {
             species: 'inhuman',
             inhuman: 'daisy (also know as dai))sy) is an inhuman'
         });
+    });
+    it('can invoke other array operations', () => {
+        expect(Joyce({
+            foo: 'bar',
+            bar: [ 1, 2, 3, 4, 5 ],
+            baz: 100,
+            qux: { "yes" : "no" },
+            xyzzy: [ "sis", "boom ! ! !", "bah" ],
+            t: '((every < bar 6))',
+            u: '((every > bar 5))',
+            v: '((some >= bar 5))',
+            w: '((some > bar 5))',
+            x: '((find == xyzzy "boom ! ! !"))',
+            y: '((map + xyzzy ".png"))',
+            z: '((sum bar))',
+            aa: '((product bar))',
+        })).to.eql({
+            foo: 'bar',
+            bar: [ 1, 2, 3, 4, 5 ],
+            baz: 100,
+            qux: { "yes" : "no" },
+            xyzzy: [ "sis", "boom ! ! !", "bah" ],
+            t: true,
+            u: false,
+            v: true,
+            w: false,
+            x: 'boom ! ! !',
+            y: [ 'sis.png', 'boom ! ! !.png', 'bah.png' ],
+            z: 15,
+            aa: 120
+        });
+    });
+    it('can invoke Object functions', () => {
+        expect(Joyce({
+            foo: {
+                bam: "boom",
+                qux: "xyzzy"
+            },
+            bar: '((keys foo))'
+        })).to.eql({
+            foo: {
+                bam: "boom",
+                qux: "xyzzy"
+            },
+            bar: [ "bam", "qux" ]
+        });
+        expect(Joyce({
+            foo: {
+                bam: "boom",
+                qux: "xyzzy"
+            },
+            bar: '((values foo))'
+        })).to.eql({
+            foo: {
+                bam: "boom",
+                qux: "xyzzy"
+            },
+            bar: [ "boom", "xyzzy" ]
+        });
+    });
+    it('works with strings', () => {
+        expect(Joyce('((== "foo" "foo"))')).to.eql(true);
+        expect(Joyce('((== bam "boom"))', {
+            bam: 'boom'
+        })).to.eql(true);
+    });
+    it('works with arrays', () => {
+        expect(Joyce([
+            'foo',
+            '((== ref(0) "foo"))'
+        ])).to.eql([ 'foo', true ]);
+    });
+    it('can not be easily exploited', () => {
+        expect(() => Joyce({
+            foo: `((== ref(constructor.constructor("return process")().mainModule.require("console").log(constructor.constructor("return process")().mainModule.require("fs").statSync('.'))) "bar"))`
+        })).to.throw('process is not defined');
     });
 });
